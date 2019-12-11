f6da567128ff669efdb3c5e810733928aaf1dbaa
Johan <johan@viopop.com>
2019-12-11 13:32:37 -0600

    testing some basics

--- fc2.rb
+++ fc2.rb
@@ -1,27 +1,29 @@
 require 'open3'
 
 MODEL_MACHINE = 'ubuntu@172.31.7.60'
+BASE_FORECAST_OUTPUT_DIR = 'Build_WRF/WRF-4.1.2/test/em_real/'
 
 LOG_FILE = File.open(File.join(__dir__, "out.log"), "a")
 
 
 def list_files
   cmd = "ssh #{MODEL_MACHINE} 'ls -tr1'"
-  LOG_FILE << "Running command...\n\t#{cmd}\n"
+  run_cmd(cmd)
+end
+
+def run_cmd(cmd)
+  log_text "Running command:\n\t\t#{cmd}\n"
   stdout, stderr, status = Open3.capture3(cmd)
-  LOG_FILE << "Time now = #{Time.now.utc}"
-  LOG_FILE << "STDOUT\n"
-  LOG_FILE << stdout
-  LOG_FILE << "\n--------------------------------------------\n"
-  LOG_FILE << "STDERR\n"
-  LOG_FILE << stdout
-  LOG_FILE << "\n--------------------------------------------\n"
-  LOG_FILE << "STDOUT\n"
-  LOG_FILE << stdout
-  LOG_FILE << "\n--------------------------------------------\n"
-  LOG_FILE << "Time now = #{Time.now.utc}"
+  log_text "STDOUT:\n#{stdout}\nSTDERR:\n#{stderr}\nSTATUS:\n#{status}\n"
+  log(log_text)
+end
+
+def log(in_text)
+  LOG_FILE << "\n#{Time.now.utc}   --------------------------\n#{in_text}\n"
 end
 
 list_files
-list_files
+
+
+
 
