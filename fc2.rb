1710fd6a3cb040e6ca92f2c3d141432345663dae
Johan <johan@viopop.com>
2019-12-11 11:26:39 -0600

    testing some basics

--- fc2.rb
+++ fc2.rb
@@ -8,8 +8,17 @@ LOG_FILE = File.open(File.join(__dir__, "out.log"), "a")
 def list_files
   cmd = "ssh #{MODEL_MACHINE} 'ls -tr1'"
   stdout, stderr, status = Open3.capture3(cmd)
+  LOG_FILE << "Time now = #{Time.now.utc}"
+  LOG_FILE << "STDOUT\n"
   LOG_FILE << stdout
-  LOG_FILE << "\n------------------------------------\n"
+  LOG_FILE << "\n--------------------------------------------\n"
+  LOG_FILE << "STDERR\n"
+  LOG_FILE << stdout
+  LOG_FILE << "\n--------------------------------------------\n"
+  LOG_FILE << "STDOUT\n"
+  LOG_FILE << stdout
+  LOG_FILE << "\n--------------------------------------------\n"
+  LOG_FILE << "Time now = #{Time.now.utc}"
 end
 
 list_files
